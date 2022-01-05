d16c2c97bee9dfe74dee9b2e55f9f04e71185033
Keyu Zhang <keyuz4@uci.edu>
2022-01-05 05:16:40 +0000

    add hw0 files

---
+++ hws/hw0/src/hw0.c
@@ -0,0 +1,23 @@
+// Your name
+// Netid
+
+
+
+#include "hw0.h"
+
+int main (int argc, char *argv[])
+{
+
+	//Comment/Delete this print statement and insert your hw0 code here 
+	printf("Hello ICS53 student!\n"); 
+
+	return 0;
+}
+
+//Function to print out a single arugment to the screen
+void printArg(char * arg_str, int pos){
+
+	//Insert your code here
+
+	return;
+}
